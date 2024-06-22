798ccb79dd844c2047882d02427e4679216f4e59
axreer <saidaxrorpc7@gmail.com>
2024-06-22 16:23:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var misol = +prompt("18+17=")
+
+if (misol !== 18 + 17) {
+    console.log(("18+17=" + misol) + " Sizning javob notog'ri togri javob " + (18 + 17));
+}
+else if (misol == 18 + 17) {
+    console.log(("18+17=" + misol) + " sizning javob tog'ri");
+}
+var misol = +prompt("18+17=")
+if (misol !== 18 + 17) {
+    console.log(("18+17=" + misol) + " Sizning javob notog'ri togri javob " + (18 + 17));
+}
+else if (misol == 18 + 17) {
+    console.log(("18+17=" + misol) + " sizning javob tog'ri");
+}
+var misol = +prompt("51-27=")
+if (misol !== 51 - 27) {
+    console.log(("51-27=" + misol) + " Sizning javob notog'ri togri javob " + (51 - 27));
+}
+else if (misol == 51 - 27) {
+    console.log(("51-27=" + misol) + " sizning javob tog'ri");
+}
+var misol = +prompt("25*55=")
+if (misol !== 25 * 55) {
+    console.log(("25*55=" + misol) + " Sizning javob notog'ri togri javob " + (25 * 55));
+}
+else if (misol == 25 * 55) {
+    console.log(("25*55=" + misol) + "sizning javob tog'ri");
+}
+var misol = +prompt("128-64=")
+if (misol !== 128 - 64) {
+    console.log(("128-64=" + misol) + " Sizning javob notog'ri togri javob " + (128 - 64));
+}
+else if (misol == 128 - 64) {
+    console.log(("128-64=" + misol) + " sizning javob tog'ri");
+};
+var misol = +prompt("256-128=")
+if (misol !== 256 - 128) {
+    console.log(("256-128=" + misol) + " Sizning javob notog'ri togri javob " + (256 - 128));
+}
+else if (misol == 256 - 128) {
+    console.log(("256-128=" + misol) + ' sizning javob togri');
+}
+
